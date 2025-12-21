e9491688b913a510e7fb0b0308a31db1fd75b9b8
triciayundaa <triciayunda1010@gmail.com>
2025-12-21 09:12:59 +0700

    vercel 15

---
+++ src/middleware/upload.js
@@ -0,0 +1,23 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+// Pastikan variabel lingkungan ini sudah ada di .env (lokal) dan Settings Vercel
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'wellbee_articles', 
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+    transformation: [{ width: 800, height: 600, crop: 'limit' }] 
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
